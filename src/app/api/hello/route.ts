52da45218c3e33cf178d8034d6bd2574b82fd1b9
tavanam rama <rcrtavanam@gmail.com>
2025-01-01 11:01:34 +0530

    add

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,98 +1,81 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { createTransfer } from '@solana/pay';
 import { Connection, Keypair, PublicKey, SystemProgram, Transaction, TransactionConfirmationStrategy } from '@solana/web3.js'
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { NextResponse } from 'next/server';
+import { useDexProgram } from "../../../components/liquidity/data-mutaion";
 
-type GetData = {
-  label: string
-  icon: string
-}
-type PostData = {
-  transaction: string,
-  message?: string
+interface GetResponse {
+    label: string;
+    icon: string;
 }
 
-function GET(
-  req: NextApiRequest,
-  res: NextApiResponse<GetData>
-  ) {
-  const label = 'SolAndy Pay';
-  const icon = 'https://avatars.githubusercontent.com/u/92437260?v=4';
+export async function GET(request: Request): Promise<NextResponse> {
+    const label = 'RCR PAYMENT';
+    const icon = 'https://avatars.githubusercontent.com/u/92437260?v=4';
+    console.log("GET REQUEST")
 
-  res.status(200).send({
-      label,
-      icon,
+    return NextResponse.json({ label, icon });
+}
+
+interface PostResponse {
+    transaction: string;
+    message?: string;
+}
+
+export async function POST(request: Request): Promise<NextResponse> {
+  const body = await request.json();
+  console.log(body)
+
+  const accountField = body.recipient;
+  if (!accountField) throw new Error('missing account');
+
+  const sender = new PublicKey(accountField);
+
+  const merchant = Keypair.fromSecretKey(
+    new Uint8Array(JSON.parse("[195,244,178,84,138,60,237,22,247,155,217,194,140,11,241,16,239,243,94,88,76,73,210,238,14,69,182,81,94,221,124,232,44,150,171,61,45,2,165,191,213,76,92,186,0,243,199,0,190,253,28,178,33,149,29,104,37,76,218,65,114,96,230,233]")),
+  );
+
+
+  // Build Transaction
+  const ix = SystemProgram.transfer({
+    fromPubkey: sender,
+    toPubkey: new PublicKey("AuEHLxepJnFXXFmXhGwXEuroW5t1b3PewGyLMhbDycdn"),
+    lamports: 133700000
+  })
+
+  let transaction = new Transaction();
+  transaction.add(ix);
+
+  const connection = new Connection("https://api.devnet.solana.com")
+  const bh = await connection.getLatestBlockhash();
+  transaction.recentBlockhash = bh.blockhash;
+  transaction.feePayer = merchant.publicKey; 
+
+  // for correct account ordering 
+  transaction = Transaction.from(transaction.serialize({
+    verifySignatures: false,
+    requireAllSignatures: false,
+  }));
+
+  transaction.sign(merchant);
+  console.log(transaction.signature?.toString());
+
+  // airdrop 1 SOL just for fun
+  connection.requestAirdrop(sender, 1000000000);
+
+  // Serialize and return the unsigned transaction.
+  const serializedTransaction = transaction.serialize({
+    verifySignatures: false,
+    requireAllSignatures: false,
   });
-}
 
-async function POST(
-  req: NextApiRequest,
-  res: NextApiResponse<PostData>
-  ) {
-    // Account provided in the transaction request body by the wallet.
-    const accountField = req.body?.account;
-    if (!accountField) throw new Error('missing account');
+  const base64Transaction = serializedTransaction.toString('base64');
+  const message = 'Thank you for using AndyPay';
 
-    const sender = new PublicKey(accountField);
+  // const strategy : TransactionConfirmationStrategy =  {
+  //   signature: transaction.
+  // }
+  // connection.confirmTransaction();
 
-    const merchant = Keypair.fromSecretKey(
-      new Uint8Array(JSON.parse("[226,230,33,166,183,94,221,240,76,0,177,119,22,166,134,93,69,185,83,121,221,13,229,219,18,55,91,84,86,112,53,87,139,130,97,105,159,216,5,167,211,57,175,154,105,195,156,4,68,100,253,224,35,32,204,44,126,175,226,176,146,254,206,226]")),
-    );
+  return NextResponse.json({ transaction: base64Transaction, message });
 
-
-    // Build Transaction
-    const ix = SystemProgram.transfer({
-      fromPubkey: sender,
-      toPubkey: new PublicKey("APaynxjiBJBrEX5rqYBTbmSFN4NhPg6TKzkTmhG7URoX"),
-      lamports: 133700000
-    })
-
-    let transaction = new Transaction();
-    transaction.add(ix);
-
-    const connection = new Connection("https://api.devnet.solana.com")
-    const bh = await connection.getLatestBlockhash();
-    transaction.recentBlockhash = bh.blockhash;
-    transaction.feePayer = merchant.publicKey; 
-
-    // for correct account ordering 
-    transaction = Transaction.from(transaction.serialize({
-      verifySignatures: false,
-      requireAllSignatures: false,
-    }));
-
-    transaction.sign(merchant);
-    console.log(transaction.signature?.toString());
-
-    // airdrop 1 SOL just for fun
-    connection.requestAirdrop(sender, 1000000000);
-
-    // Serialize and return the unsigned transaction.
-    const serializedTransaction = transaction.serialize({
-      verifySignatures: false,
-      requireAllSignatures: false,
-    });
-
-    const base64Transaction = serializedTransaction.toString('base64');
-    const message = 'Thank you for using AndyPay';
-
-    // const strategy : TransactionConfirmationStrategy =  {
-    //   signature: transaction.
-    // }
-    // connection.confirmTransaction();
-
-    res.status(200).send({ transaction: base64Transaction, message });
-
-}
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<GetData|PostData>
-) {
-  if(req.method == "GET"){
-    console.log("received GET request");
-    return GET(req, res);
-  } else if(req.method == "POST"){
-    console.log("received POST request");
-    return await POST(req, res);
-  }
 }
